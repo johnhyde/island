162ebcbb9f2828d2d74d77bb519dfc20ba8567e3
Mernon Squirnon <mernonsquirnon@protonmail.com>
2025-11-21 03:15:37 -0800

    be clearer about what annotation-type things are included

--- wordcount.sh
+++ wordcount.sh
@@ -2,4 +2,4 @@ echo Chapters:
 cat [0-9][0-9]*.md | ./strip-annotations.py | wc -w
 echo Appendices:
 cat A[0-9]*.md | ./strip-annotations.py | wc -w
-echo Note that these counts do NOT include annotations 🙂
+echo 'Note that these counts do not include annotations, but do include [X; foo words] type things (due to inconvenience of removing them).'
