967db41c68f293581d99b3da30063540d6c84e71
Mernon Squirnon <mernonsquirnon@protonmail.com>
2025-12-20 02:12:23 -0800

    add wordcount adding step to build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ echo '<pre>' > "A9 wordcounts.jd"
 ./wordcount.sh >> "A9 wordcounts.jd"
 echo '</pre>' >> "A9 wordcounts.jd"
 
+# Is this the "wrong way" to do this? Idk.
+git add "A9 wordcounts.jd"
+
 # Find all johndown files, format them as JSON, and build the final JSON file
 find . -maxdepth 1 -name '[0-A][0-9]*.jd' | sort -V | \
   jq -R '{"filename": ., "title": sub("^./[0-A][0-9]* "; "") | sub("\\.jd$"; "")}' | \
