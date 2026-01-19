d1e7cbff75e3b343a5138fcb7e8a1a25f6fcaadb
Mernon Squirnon <mernonsquirnon@protonmail.com>
2026-01-18 17:49:53 -0800

    refactor the python script to do all of the word count, speeding up the pre-commit substanstially
    This also removes the ability for the annotation stripper to read from stdin, since I never used that anyway. Actually, the main functionality of the annotation stripper is now untested, so that's cool.

--- strip-annotations.py
+++ strip-annotations.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 
-count_mode = False
+sys.stdout.reconfigure(encoding="utf-8") #important.
 
-def complain():
-  sys.stderr.write("ERROR: I only handle stdin and cannot take arguments. Except --count.\n")
-  exit(-1)
-if len(sys.argv) > 2:
-  complain()
+parser = argparse.ArgumentParser()
+parser.add_argument('--count', action='store_true')
+parser.add_argument('files', nargs='+')
+args = parser.parse_args()
 
-if len(sys.argv) > 1:
-  if sys.argv[1] == "--count":
-    count_mode = True
-  else:
-    complain()
+running_total_count = 0
 
-data = sys.stdin.read()
-n = len(data)
-annotation = 0
-i = 0
-running_count = 0
-was_just_whitespace = True # the beginning of the file is effectively whitespace
+count_mode = args.count
+files = args.files
+#print(files)
+for file in files:
+    #print(file)
+    data = open(file, encoding="utf-8").read()
+    n = len(data)
+    annotation = 0
+    i = 0
+    running_count = 0
+    was_just_whitespace = True # the beginning of the file is effectively whitespace
 
-"""The wc utility shall consider a word to be a non-zero-length string of characters delimited by white space.
-—https://pubs.opengroup.org/onlinepubs/9799919799/utilities/wc.html
-"""
+    """The wc utility shall consider a word to be a non-zero-length string of characters delimited by white space.
+    —https://pubs.opengroup.org/onlinepubs/9799919799/utilities/wc.html
+    """
 
-while i < n:
-    c = data[i]
-    if c == '[' and i + 1 < n and data[i+1] == '@':
-        annotation += 1
-        i += 2
-        continue
-    if annotation == 0:
-        if count_mode:
-            if c.isspace():
-                was_just_whitespace = True
+    while i < n:
+        c = data[i]
+        if c == '[' and i + 1 < n and data[i+1] == '@':
+            annotation += 1
+            i += 2
+            continue
+        if annotation == 0:
+            if count_mode:
+                if c.isspace():
+                    was_just_whitespace = True
+                else:
+                    if was_just_whitespace:
+                        running_count+=1
+                    was_just_whitespace = False
             else:
-                if was_just_whitespace:
-                    running_count+=1
-                was_just_whitespace = False
-        else:
-            sys.stdout.write(c)
-    if c == ']' and annotation > 0:
-        annotation -= 1
-    i += 1
+                sys.stdout.write(c)
+        if c == ']' and annotation > 0:
+            annotation -= 1
+        i += 1
 
-if count_mode:
-  sys.stdout.write(str(running_count)+"\n")
+    if count_mode:
+        sys.stdout.write("\t"+str(running_count)+"\t"+file+"\n", )
+    running_total_count += running_count
+print(f"{running_total_count}\ttotal")
--- wordcount.sh
+++ wordcount.sh
@@ -1,19 +1,5 @@
-annocount() {
-  name="${2:-$1}" #use 1 as name unless 2 is provided
-  printf "\t" # It seems like some versions of wc prefix with a tab and some don't. Let's prefix with a tab in a cross-platform-alicious way.
-  cat "$1" | ./strip-annotations.py --count | tr -d '\r\n\t'
-  printf "\t$name\n"
-}
-
 echo Chapters:
-for x in [0-9][0-9]*.jd
-do
-  annocount "$x"
-done
-#this is hard to abstract to the function because of glob reasons:
-cat [0-9][0-9]*.jd | ./strip-annotations.py | wc -w | tr -d '\n'
-printf "\ttotal\n"
+./strip-annotations.py --count [0-9][0-9]*.jd | tr -d '\r'
 echo Appendices:
-cat A[0-9]*.jd | ./strip-annotations.py | wc -w
-# With regards to getting a more-specific count of these... eh... who cares...
+./strip-annotations.py --count A[0-9]*.jd | tr -d '\r'
 echo 'Note that these counts do not include annotations, but do include [X; foo words] type things (due to inconvenience of removing them).'
