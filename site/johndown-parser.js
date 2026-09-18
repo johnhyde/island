a78cc9699b138f9dfe787556620192c71b15b14c
Mernon Squirnon <mernonsquirnon@protonmail.com>
2026-09-18 15:13:33 -0700

    also do that for diamond marker

--- site/johndown-parser.js
+++ site/johndown-parser.js
@@ -344,7 +344,7 @@ class JohndownParser {
   }
 
   processQuotes(text) {
-    text = text.replace(/(?<!(?:^|[\s\n\(\^])—?)"/g, "”");
+    text = text.replace(/(?<!(?:^|[\s\n\(\^])◆?—?)"/g, "”");
     text = text.replace(/"/g, "“");
     text = text.replace(/'/g, "’");
     return text;
