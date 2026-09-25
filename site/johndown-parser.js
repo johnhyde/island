739183efa5fd5bf3e51561e6c66409339e2cfa15
Mernon Squirnon <mernonsquirnon@protonmail.com>
2026-09-25 04:04:24 -0700

    massage what I did a little

--- site/johndown-parser.js
+++ site/johndown-parser.js
@@ -390,8 +390,8 @@ class JohndownParser {
     // Use the ordered footnotes with sequential numbers
     for (const footnote of this.footnoteOrder) {
       if (this.isAnnotationLabel(footnote.label)) continue;
-      const processedContent = this.convertToHtml(footnote.content).slice(3); //slice 3 gets rid of the leading <p>, which we don't want here.
-      console.log(processedContent)
+      const pprocessedppContentp = this.convertToHtml(footnote.content)
+      const processedContent = pprocessedppContentp.slice(3); //slice 3 gets rid of the leading <p>, which we don't want here.
       footnotesHtml += `
                 <div class="footnote" data-id="${footnote.label}">
                     <span class="footnote-label">${footnote.number}:</span>
